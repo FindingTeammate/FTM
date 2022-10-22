d9c010e53b4977baa0169372796b0d2b0f29efcf
Abhishek Keri <111935093+keri39@users.noreply.github.com>
2022-10-22 00:04:08 -0500

    change settings for PythonAnywhere

--- FindingTeamMates/settings.py
+++ FindingTeamMates/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2#sav=jmvbp)75fxa#)6k&vpq7-&3^v$#*v&u$(aak-=qd$07%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
