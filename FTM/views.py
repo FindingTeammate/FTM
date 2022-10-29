4b0e30058063cff0fd342d961c7f0f3d3a0da2f0
Abhishek Keri <111935093+keri39@users.noreply.github.com>
2022-10-28 21:20:40 -0500

    Update views.py

--- FTM/views.py
+++ FTM/views.py
@@ -11,15 +11,6 @@ from rest_framework import viewsets
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 
-# class UserDetailAPI(APIView):
-#   authentication_classes = (TokenAuthentication,)
-#   permission_classes = (AllowAny,)
-#   def get(self,request,*args,**kwargs):
-#     user = User.objects.get(id=request.user.id)
-#     serializer = UserSerializer(user)
-#     return Response(serializer.data)
-
-
 class LoginView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
     
