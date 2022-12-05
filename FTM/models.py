312f2ef2367bac6480c9a4b6df14a1b10c239451
Abhishek Keri <111935093+keri39@users.noreply.github.com>
2022-12-05 12:49:12 -0600

    Update models.py

--- FTM/models.py
+++ FTM/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
-
-# from django.template.backends import django
-
 from django.template.backends import django
 
 User = get_user_model()
